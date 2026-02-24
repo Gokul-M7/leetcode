82847837215d0a9777da7f47507a4ade35584853
GOKUL M <gokulvm2005@gmail.com>
2026-02-24 22:33:39 +0530

    Time: 1 ms (73.96%) | Memory: 43.1 MB (10.94%) - LeetSync

---
+++ 1999-longer-contiguous-segments-of-ones-than-zeros/longer-contiguous-segments-of-ones-than-zeros.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkZeroOnes(String s) {
+        int c1=0,c2=0,max1=0,max2=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='1')
+            {
+                c1++;
+                c2=0;
+                max1=Math.max(c1,max1);
+            }else
+            {
+                c2++;
+                c1=0;
+                max2=Math.max(c2,max2);
+            }
+        }
+        return max1>max2;
+    }
+}
