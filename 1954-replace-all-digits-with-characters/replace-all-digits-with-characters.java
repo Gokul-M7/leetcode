f4b668ccd8e8c04eba5a249e82eba89aec16ac4f
GOKUL M <gokulvm2005@gmail.com>
2026-02-25 22:31:18 +0530

    Time: 1 ms (53.58%) | Memory: 43 MB (68.76%) - LeetSync

---
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String replaceDigits(String s) {
+       StringBuilder sb=new StringBuilder();
+       int n=s.length();
+       for(int i=0;i<n;i++)
+       { 
+        if(i%2==0)
+        {
+            sb.append(s.charAt(i));
+        }else
+        {
+        sb.append((char)(s.charAt(i-1)+(s.charAt(i)-'0')));
+        }
+       }
+        return sb.toString();
+    }
+}
